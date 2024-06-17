f7cae24e1c75a94d49c69b55d0bfe8915ea346ac
VasundharaShivankar <vasundharashivankar179@gmail.com>
2024-06-17 10:29:08 +0530

    navbar corrections

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 function Navbar(){
     return(
         <div className="navbar">
-            <link to='/' className="navbar-content">Home</link>
-            <link to='/about'  className="navbar-content">About</link>
-            <link to='/contact'  className="navbar-content">Contact</link>
+            <Link to='/' className="navbar-content">Home</Link>
+            <Link to='/about'  className="navbar-content">About</Link>
+            <Link to='/contact'  className="navbar-content">Contact</Link>
         </div>
     );
 }
